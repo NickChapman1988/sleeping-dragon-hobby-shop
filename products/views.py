989b0145209deed616298bf18b5b638aca4db59a
NickChapman1988 <muppetnj_uk@hotmail.com>
2021-11-20 11:19:07 +0000

    Add brand filtering

--- products/views.py
+++ products/views.py
@@ -12,13 +12,22 @@ def all_products(request):
     products = Product.objects.all()
     query = None
     categories = None
+    brand = None
 
     if request.GET:
+        # filter by product brand
+        if 'brand' in request.GET:
+            brand = request.GET['brand']
+            products = Product.objects.filter(brand=brand)
+            brand = Product.objects.filter(brand__in=products)
+
+        # filter by product category
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
-            category = Category.objects.filter(name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
+        # filter by search query
         if 'query' in request.GET:
             query = request.GET['query']
             if not query:
@@ -32,6 +41,7 @@ def all_products(request):
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'current_brand': brand,
     }
 
     return render(request, 'products/products.html', context)
