3f06db05389ca600c57e030c1d8d05bedd0a3efa
NickChapman1988 <muppetnj_uk@hotmail.com>
2021-11-20 10:23:43 +0000

    Add search functionality

--- products/views.py
+++ products/views.py
@@ -1,16 +1,30 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Product
+# Adapted from Boutique Ado walkthrough project by Code Institute
 
-# Create your views here.
+from django.shortcuts import render, reverse, redirect, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from .models import Product
 
 
 def all_products(request):
     """ A view to show all products, including sorting and searching """
 
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'query' in request.GET:
+            query = request.GET['query']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
