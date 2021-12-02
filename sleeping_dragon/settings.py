c52637061fc071d58e9ae85eca7578b3e2d94fd0
NickChapman1988 <muppetnj_uk@hotmail.com>
2021-12-02 20:59:11 +0000

    base.css and login/register refactor

--- sleeping_dragon/settings.py
+++ sleeping_dragon/settings.py
@@ -202,7 +202,7 @@ DEFAULT_FROM_EMAIL = 'nick@sleepingdragonhobbyshop.co.uk'
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'nick@sleepingdragonhobbyshop.co.uk'
-else: 
+else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
