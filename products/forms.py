8142a2ad98e04145fd196a275b4dfe8a2f27adba
NickChapman1988 <muppetnj_uk@hotmail.com>
2021-12-06 21:27:11 +0000

    Add edit review link to product detail page

--- products/forms.py
+++ products/forms.py
@@ -50,7 +50,7 @@ class ReviewForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['content'].widget.attrs['autofocus'] = True
-        self.fields['rating'].widget.attrs['min'] = 0
+        self.fields['rating'].widget.attrs['min'] = 1
         self.fields['rating'].widget.attrs['max'] = 5
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0 review-input'
