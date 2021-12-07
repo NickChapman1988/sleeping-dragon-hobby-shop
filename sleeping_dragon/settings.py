c5ec9c8b1a3cf0d0926f6fa5bd95d2c053849935
NickChapman1988 <muppetnj_uk@hotmail.com>
2021-12-07 15:58:40 +0000

    Update Database settings

--- sleeping_dragon/settings.py
+++ sleeping_dragon/settings.py
@@ -107,17 +107,11 @@ WSGI_APPLICATION = 'sleeping_dragon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
