413c2a7692571c7c7e5e9f909d3d441b7af6b992
NickChapman1988 <muppetnj_uk@hotmail.com>
2021-12-13 21:10:50 +0000

    Add products app views testing

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class TestHomeViews(TestCase):
+    def test_landing_page(self):
+        """ Tests loading the landing page """
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/index.html')
+        
---
+++ products/test_views.py
@@ -0,0 +1,126 @@
+from django.shortcuts import get_object_or_404
+from django.test import TestCase
+from django.urls import resolve
+from profiles.models import UserProfile
+from .models import Product, Category, Review
+from django.contrib.auth.models import User
+
+
+class TestProductsViews(TestCase):
+    
+    def test_get_all_products(self):
+        found = resolve('/products/')
+        self.assertEqual(found.url_name, "products")
+
+    def test_get_product_detail(self):
+        product = Product.objects.create(
+            name = 'Test Product',
+            description = 'This is a test product',
+            stock = 99,
+            weight = 1.0,
+            length = 1.0,
+            width = 1.0,
+            height = 1.0,
+            price = 1.0,
+        )
+        found = resolve(f'/products/{product.id}/')
+        self.assertEqual(found.url_name, "product_detail")
+
+    def test_get_product_mgmt_page(self):
+        found = resolve('/products/product_management/')
+        self.assertEqual(found.url_name, "product_management")
+
+    def test_get_review_product_page(self):
+        product = Product.objects.create(
+            name = 'Test Product',
+            description = 'This is a test product',
+            stock = 99,
+            weight = 1.0,
+            length = 1.0,
+            width = 1.0,
+            height = 1.0,
+            price = 1.0,
+        )
+        review = Review.objects.create(
+            product = product,
+            rating = 5,
+            content = 'This is a test review'
+        )
+        found = resolve(f'/products/review_product/{review.id}/')
+        self.assertEqual(found.url_name, "review_product")
+
+    def test_can_add_product(self):
+        found = resolve('/products/add/')
+        self.assertEqual(found.url_name, "add_product")
+
+    def test_can_edit_product(self):
+        product = Product.objects.create(
+            name = 'Test Product',
+            description = 'This is a test product',
+            stock = 99,
+            weight = 1.0,
+            length = 1.0,
+            width = 1.0,
+            height = 1.0,
+            price = 1.0,
+        )
+        found = resolve(f'/products/edit/{product.id}/')
+        self.assertEqual(found.url_name, "edit_product")
+
+    def test_can_delete_product(self):
+        product = Product.objects.create(
+            name = 'Test Product',
+            description = 'This is a test product',
+            stock = 99,
+            weight = 1.0,
+            length = 1.0,
+            width = 1.0,
+            height = 1.0,
+            price = 1.0,
+        )
+        found = resolve(f'/products/delete/{product.id}/')
+        self.assertEqual(found.url_name, "delete_product")
+
+    def test_can_add_category(self):
+        found = resolve('/products/add_category/')
+        self.assertEqual(found.url_name, "add_category")
+
+    def test_can_edit_category(self):
+        category = Category.objects.create(
+            name = 'test_category',
+            display_name = 'Test Category'
+        )
+        found = resolve(f'/products/product_management/edit_category/{category.id}/')
+        self.assertEqual(found.url_name, "edit_category")
+
+    def test_can_delete_category(self):
+        category = Category.objects.create(
+            name = 'test_category',
+            display_name = 'Test Category'
+        )
+        found = resolve(f'/products/product_management/delete_category/{category.id}/')
+        self.assertEqual(found.url_name, "delete_category")
+
+    def test_can_delete_review(self):
+        user = User.objects.create(
+            username = 'test'
+        )
+        product = Product.objects.create(
+            name = 'Test Product',
+            description = 'This is a test product',
+            stock = 99,
+            weight = 1.0,
+            length = 1.0,
+            width = 1.0,
+            height = 1.0,
+            price = 1.0,
+        )
+        review = Review.objects.create(
+            product = product,
+            rating = 5,
+            content = 'This is a test review',
+            user = user
+        )
+        found = resolve(f'/products/delete_review/{review.id}/{user.id}/')
+        self.assertEqual(found.url_name, "delete_review")
+    
